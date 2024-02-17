f42fccfcf661bf851b5ddbc303543b70f4df205b
Malik Ahmad Mukhtar <158511475+malikahmadmukhtar@users.noreply.github.com>
2024-02-17 14:50:25 +0500

    Removed my private whatsapp number from about.dart

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -112,7 +112,7 @@ class _AboutPageState extends State<AboutPage> {
                     ),
                     GestureDetector(
                       onTap: () {
-                        var url = Uri.parse("https://wa.me/+923462369945");
+                        var url = Uri.parse("https://wa.me/");
                         launchUrl(url);
                       },
                       child: Image.asset(
